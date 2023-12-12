01822739c9a844719a7028c4d4c5bf8fb9b83279
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-12-11 17:28:57 -0800

    Add files via upload

--- Bookworm/Bookworm/Book.swift
+++ Bookworm/Bookworm/Book.swift
@@ -5,4 +5,24 @@
 //  Created by Brandon Johns on 12/11/23.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+    
+    init(title: String, author: String, genre: String, review: String, rating: Int) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating
+    }
+    
+}
--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -5,14 +5,73 @@
 //  Created by Brandon Johns on 12/10/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author),
+                 ]) var books: [Book]
+    
+    
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            IntroductionToSwiftDataAndSwiftUI()
+        NavigationStack {
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    
+                }
+                .onDelete(perform: deleteBooks)
+                
+            }
+            .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                DetailView(book: book)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Book", systemImage: "plus") {
+                        showingAddScreen.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
         }
-        .padding()
+        
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            //find book
+            
+            let book = books[offset]
+            
+            //delete
+            modelContext.delete(book)
+        }
     }
 }
 
