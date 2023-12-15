cf928c1d3ff168298bb94f47a2fa5c121c9d0f4b
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-12-15 15:26:02 -0800

    Add files via upload

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -5,17 +5,61 @@
 //  Created by Brandon Johns on 12/13/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                Spacer()
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                }
+                Spacer()
+                
+            }
         }
-        .padding()
+        .padding([.horizontal, .bottom])
+        .navigationTitle("InstaFilter")
+        .onChange(of: selectedItem, loadImage)
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else  {return}
+            guard let inputImage = UIImage(data: imageData) else {return}
+            
+            // more
+        }
     }
 }
 
