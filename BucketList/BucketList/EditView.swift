3638ee49d54e76e938c12b7f22ee059df84d3a71
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-03-27 14:59:27 -0700

    Add files via upload

--- BucketList/BucketList/EditView.swift
+++ BucketList/BucketList/EditView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct EditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     var location: Location
     var onSave: (Location) -> Void
     @State private var name: String
     @State private var description: String
 
-    init(location: Location, onSave: @escaping (Location) -> Void) {
-        self.location = location
-        self.onSave = onSave
-
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
-    }
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
+    
     
     var body: some View {
         NavigationStack {
@@ -28,6 +29,25 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                
+                Section {
+                    Section("Nearby…") {
+                        switch loadingState {
+                        case .loaded:
+                            ForEach(pages, id: \.pageid) { page in
+                                Text(page.title)
+                                    .font(.headline)
+                                + Text(": ") +
+                                Text(page.description)
+                                    .italic()
+                            }
+                        case .loading:
+                            Text("Loading…")
+                        case .failed:
+                            Text("Please try again later.")
+                        }
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -42,8 +62,45 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+            
         }
     }
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.location = location
+        self.onSave = onSave
+
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
+        }
+    }
+
 }
 
 #Preview {
